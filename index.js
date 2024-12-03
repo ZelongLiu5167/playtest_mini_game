bb51bb178930e6cd8551c8ff602163ed5bd5c00b
ZelongLiu5167 <zl5167@nyu.edu>
2024-12-04 02:57:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ app.use(express.json());
 app.post('/submit-score', async (req, res) => {
     const { score } = req.body;
 
-    // 将分数记录添加到数据库
+
     db.data.gameScores.push({
         score,
-        timeStamp: new Date().toISOString() // 自动记录时间戳
+        timeStamp: new Date().toISOString()
     });
-    await db.write(); // 写入数据库
+    await db.write();
 
     res.json({ message: 'Score submitted successfully' });
 });
 
-// 路由 - 获取所有分数记录
+
 app.get('/get-scores', async (req, res) => {
-    await db.read(); // 读取数据库内容
+    await db.read();
     res.json({ scores: db.data.gameScores });
 });
 
